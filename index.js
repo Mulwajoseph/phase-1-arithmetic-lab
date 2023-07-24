c94fee19ff3aaafc2eb68b3adcd5b9d57faa9f7e
mulwajoseph <mulwajose.jm@gmail.com>
2023-07-24 06:22:43 +0300

    arithmentics

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+// Write you
+// Challenge 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Challenge 2
+let random = Math.ceil(Math.random() * 100); // Generate a random integer between 1 and 100
+
+// Challenge 3
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Challenge 4
+let numbers = [10, 5, 20, 7];
+let max = Math.max(...numbers);
+
+console.log("Challenge 1: multiply =", multiply);
+console.log("Challenge 2: random =", random);
+console.log("Challenge 3: mod =", mod);
+console.log("Challenge 4: max =", max);
